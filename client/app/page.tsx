f405c219275de093c319e0edbdd9dc7ef4a30b94
Anjali Negi <113291155+Anjali4402@users.noreply.github.com>
2025-12-23 18:29:29 +0530

    project setup with next js, Tailwind css and shadcn

---
+++ client/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center font-sans dark:bg-black">
+      <h1>Project Setup</h1>
+      <Button >Shadcn Btn </Button>
+    </div>
+  );
+}
