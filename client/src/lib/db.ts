d11a4f86afbe5b7cabe2be38309bfb1f66f630d1
Anjali Negi <113291155+Anjali4402@users.noreply.github.com>
2026-01-06 21:33:57 +0530

    fixed issue in connecting database

--- client/src/lib/db.ts
+++ client/src/lib/db.ts
@@ -1,8 +1,12 @@
-const { username, password } = process.env;
+const DB_USERNAME = process.env.MONGODB_USERNAME;
+const DB_PASSWORD = process.env.MONGODB_PASSWORD;
 
-export const connectionStr =
-  "mongodb+srv://" +
-  username +
-  ":" +
-  password +
-  "@cluster0.c4ya5hi.mongodb.net/taskBoard?appName=Cluster0";
+if (!DB_USERNAME || !DB_PASSWORD) {
+  throw new Error("Missing MongoDB environment variables");
+}
+
+export const connectionStr = `mongodb+srv://${encodeURIComponent(
+  DB_USERNAME,
+)}:${encodeURIComponent(
+  DB_PASSWORD,
+)}@cluster0.c4ya5hi.mongodb.net/taskBoard?appName=Cluster0`;
